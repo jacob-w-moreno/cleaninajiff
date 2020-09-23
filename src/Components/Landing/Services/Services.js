9f8fcf08e18e01952653308efd64f1e6e0e39c9b
Jake <jacob.w.moreno@gmail.com>
2020-09-23 00:59:07 -0600

    Still working on styling.

--- src/Components/Landing/Services/Services.js
+++ src/Components/Landing/Services/Services.js
@@ -1,22 +1,58 @@
 import React from 'react';
-import Service from './Service';
 import CTA from '../../Buttons/CTA';
 
 const Services = (props) => {
   return (<div className='services'>
-    <h2>SERVICES</h2>
-    <div className='services__grid'>
-      <Service
-        name='Small Vehicle Interior Detail'
-        price='$130'/>
-      <Service
-        name='Medium Vehicle Interior Detail'
-        price='$160'/>
-      <Service
-        name='Large Vehicle Interior Detail'
-        price='$100'/>
+    <div className='services__section'>
+      <h2>SERVICES</h2>
+      <h3>Interior Detailing</h3>
+      <ul>
+        <li>Hard interior surfaces wiped down</li>
+        <li>Door jambs wiped and cleaned</li>
+        <li>Carpet, seats, and compartments vacuumed</li>
+        <li>Minor carpet stain removal</li>
+        <li>Plastic and vinyl interior components UV protected</li>
+        <li>Glass cleaned inside and out to a streak-free finish</li>
+      </ul>
+      <h3>Exterior Detailing</h3>
+      <ul>
+        <li>Something</li>
+        <li>Something else</li>
+        <li>Another thing</li>
+      </ul>
     </div>
-    <CTA cta='CALL TO ACTION'/>
+    <div className='services__section'>
+      <h2>PRICING</h2>
+      <h3>Compact / Sedan Interior Detail:</h3><span>$130</span>
+      <h3>SUV-Sized Interior Detail: $160</h3><span>$130</span>
+      <h3>Truck / Larger Vehicles: $180 +</h3><span>$130</span>
+    </div>
+    {/* <h2>SERVICES & PRICING</h2>
+    <section className='services__grid'>
+      <div className="services__card">
+        <h3>Interior Detailing</h3>
+        <ul>
+          <li>All hard interior surfaces wiped down</li>
+          <li>Door jambs wiped and cleaned</li>
+          <li>Carpet, seats, and compartments vacuumed</li>
+          <li>Minor carpet stain removal</li>
+          <li>All plastic and vinyl interior components UV protected</li>
+          <li>Glass cleaned inside and out to a streak-free finish</li>
+        </ul>
+      </div>
+      <div className="services__card">
+        <h3>Pricing</h3>
+        <ul>
+          <li>All hard interior surfaces wiped down</li>
+          <li>Door jambs wiped and cleaned</li>
+          <li>Carpet, seats, and compartments vacuumed</li>
+          <li>Minor carpet stain removal</li>
+          <li>All plastic and vinyl interior components UV protected</li>
+          <li>Glass cleaned inside and out to a streak-free finish</li>
+        </ul>
+      </div>
+    </section>
+    <CTA cta='CALL TO ACTION'/> */}
   </div>)
 }
 export default Services
